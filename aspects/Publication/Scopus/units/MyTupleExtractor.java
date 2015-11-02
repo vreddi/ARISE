6f5a74bfcbeeaf05e921be98c60058e08066a6ad
Vishrut Reddi <vishrutreddi@gmail.com>
2015-11-02 13:26:21 -0600

    Sources Added [Publication] and new module: Analyzer added
    * The new sources include Scopus, Science Direct and Microsoft
    Academics. Their API KEYS are hidden. These sources won’t work as the
    getter does not have the API-Keys. For the keys generate your own or
    ask Vishrut (@vreddi).
    
    * New module graphAnalyzer has been added in the controllers. This
    provides a visual analysis to the tool to see the spread of researchers
    representation over the different aspects and the sources. It works
    with all existing and upcoming sources. No new module call needs to be
    made to activate the service for graphAnalyzer.

--- aspects/Publication/Scopus/units/MyTupleExtractor.java
+++ aspects/Publication/Scopus/units/MyTupleExtractor.java
@@ -1,20 +1,31 @@
+
+import controllers.graphAnalyzer.GlobalGraphInfo;
 import controllers.wrapper.sourceWrapper.interfaces.TupleExtractor;
+
 import net.sf.json.JSON;
 import net.sf.json.JSONArray;
 import net.sf.json.JSONObject;
 
 import java.lang.Exception;
 import java.lang.Override;
+import java.lang.System;
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+import static controllers.graphAnalyzer.GraphAnalyzer.*;
 
 public class MyTupleExtractor implements TupleExtractor{
 
     @Override
     public JSONArray getTuples(JSON all) {
         JSONObject allAsObject = JSONObject.fromObject(all);
+
         try {
-            return allAsObject.getJSONArray("entry");
+            JSONObject innerJSON = JSONObject.fromObject(allAsObject.get("search-results"));
+            return innerJSON.getJSONArray("entry");
         } catch (Exception e) {
-            JSONObject rec = allAsObject.getJSONObject("entry");
+            JSONObject innerJSON = JSONObject.fromObject(allAsObject.get("search-results"));
+            JSONObject rec = innerJSON.getJSONObject("entry");
             JSONArray ret = new JSONArray();
             ret.add(rec);
             return ret;
